38e6481c03e7493bcb89c1a2c1e9e342b4c0c32c
yellowsea <yunhai0644@163.com>
2022-05-17 14:48:49 +0800

    chore:  app-footer style  finish

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -68,4 +68,175 @@
 </script>
 
 <style lang="less" scoped>
+@import "../assets/styles/variables.less";
+
+.app-footer {
+  overflow: hidden;
+  background: #f5f5f5;
+  padding-top: 20px;
+
+  .contact {
+    background: #fff;
+
+    .container {
+      padding: 60px 0 40px 25px;
+      display: flex;
+
+      dl {
+        height: 190px;
+        text-align: center;
+        padding: 0 72px;
+        border-right: 1px solid #f2f2f2;
+        color: #999;
+
+        &:first-child {
+          padding-left: 0;
+        }
+
+        &:last-child {
+          border-right: none;
+          padding-right: 0;
+        }
+      }
+
+      dt {
+        line-height: 1;
+        font-size: 18px;
+      }
+
+      dd {
+        // 设置dd样式
+        margin: 36px 12px 0 0;
+        // 容器局 左
+        float: left;
+        // 设置宽高
+        width: 92px;
+        height: 92px;
+        // 内边距
+        padding-top: 10px;
+        border: 1px solid #ededed;
+
+        .iconfont {
+          font-size: 36px;
+          // 设置块级
+          display: block;
+          color: #666;
+        }
+
+        &:hover {
+          .iconfont {
+            color: @xtxColor;
+          }
+        }
+
+        &:last-child {
+          margin-right: 0;
+        }
+      }
+
+      // 单独设置 dd 中的内容， 相同则会覆盖
+      .qrcode {
+        width: 92px;
+        height: 92px;
+        padding: 7px;
+        border: 1px solid #ededed;
+      }
+
+      .download {
+        padding-top: 5px;
+        font-size: 14px;
+        width: auto;
+        height: auto;
+        border: none;
+
+        span {
+          display: block;
+        }
+
+        a {
+          display: block;
+          line-height: 1;
+          padding: 10px 25px;
+          margin-top: 5px;
+          color: #fff;
+          border-radius: 2px;
+          background-color: @xtxColor;
+        }
+      }
+
+      .hotline {
+        padding-top: 20px;
+        font-size: 20px;
+        color: #666;
+        width: auto;
+        height: auto;
+        border: none;
+
+        small {
+          display: block;
+          font-size: 15px;
+          color: #999;
+        }
+      }
+    }
+  }
+
+  .extra {
+    background: #333;
+
+    .slogan {
+      height: 178px;
+      line-height: 58px;
+      padding: 60px 100px;
+      border-bottom: 1px solid #434343;
+      display: flex;
+      justify-content: space-between;
+
+      a {
+        color: #fff;
+        height: 58px;
+        line-height: 58px;
+        font-size: 28px;
+
+        i {
+          font-size: 50px;
+          vertical-align: middle;
+          margin-right: 10px;
+          font-weight: 100;
+        }
+
+        span {
+          vertical-align: middle;
+          text-shadow: 0 0 1px #333;
+        }
+      }
+    }
+
+    .copyright {
+      height: 170px;
+      padding-top: 40px;
+      text-align: center;
+      color: #999;
+      font-size: 15px;
+
+      p {
+        line-height: 1;
+        margin-bottom: 20px;
+      }
+
+      a {
+        color: #999;
+        line-height: 1;
+        // 设置边距
+        padding: 0 10px;
+        border-right: 1px solid #999;
+
+        &:last-child {
+          border-right: none;
+
+        }
+      }
+    }
+  }
+}
 </style>
