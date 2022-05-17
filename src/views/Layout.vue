169c70ff9b2b549fa9595dea95b97993dcf2ebd0
yellowsea <yunhai0644@163.com>
2022-05-17 13:53:49 +0800

    chore:  app-footer  layout

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -8,12 +8,14 @@
     <!-- 二级路由 -->
     <router-view />
   </main>
-  <footer>公共底部</footer>
+  <!-- <footer>公共底部</footer> -->
+  <AppFooter />
 
 </template>
 <script lang="ts" setup>
 import AppTopNav from '@/components/app-topnav.vue'
 import AppHeader from '@/components/app-header.vue'
+import AppFooter from '@/components/app-footer.vue'
 </script>
 <style lang="less">
   
