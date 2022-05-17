d936ff869203991efea76b58e88014fbae231e30
yellowsea <yunhai0644@163.com>
2022-05-17 16:49:27 +0800

    chore:  app-header-nav  update

---
+++ src/components/app-header-nav.vue
@@ -0,0 +1,115 @@
+<template>
+  <ul class="app-header-nav">
+    <li class="home">
+      <router-link to="/">首页</router-link>
+    </li>
+    <li><a href="#">美食</a>
+      <!-- 列表下的img -->
+      <div class="layer">
+        <ul>
+          <li v-for="i in 10" :key="i">
+            <a href="#">
+              <img src="http://zhoushugang.gitee.io/erabbit-client-pc-static/uploads/img/category%20(4).png" alt="">
+              <p>果干</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    <li><a href="#">餐厨</a></li>
+    <li><a href="#">艺术</a></li>
+    <li><a href="#">电器</a></li>
+    <li><a href="#">居家</a></li>
+    <li><a href="#">洗护</a></li>
+    <li><a href="#">孕婴</a></li>
+    <li><a href="#">服装</a></li>
+    <li><a href="#">杂货</a></li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less" scoped>
+@import "../assets/styles/variables.less";
+
+.app-header-nav {
+  width: 820px;
+  display: flex;
+  justify-content: space-around;
+  padding-left: 40px;
+  position: relative;
+  z-index: 998;
+
+  >li {
+    margin-right: 40px;
+    width: 38px;
+    text-align: center;
+
+    >a {
+      font-size: 16px;
+      line-height: 32px;
+      height: 32px;
+      display: inline-block;
+    }
+
+    &:hover {
+      >a {
+        color: @xtxColor;
+        border-bottom: 1px solid @xtxColor;
+      }
+
+      // 子分类
+      >.layer {
+        height: 132px;
+        opacity: 1;
+      }
+    }
+  }
+}
+
+
+// 子分类
+
+.layer {
+  width: 1240px;
+  background: #fff;
+  position: absolute;
+  left: -200px;
+  top: 56px;
+  height: 0;
+  overflow: hidden;
+  opacity: 0;
+  box-shadow: 0 0 5px #ccc;
+  transition: all .2s .1s;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 70px;
+    align-items: center;
+    height: 132px;
+
+    li {
+      width: 110px;
+      text-align: center;
+
+      img {
+        width: 60px;
+        height: 60px;
+      }
+
+      p {
+        padding-top: 10px;
+      }
+
+      &:hover {
+        P {
+          color: @xtxColor
+        }
+      }
+    }
+  }
+}
+</style>
--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -4,20 +4,8 @@
       <h1 class="logo">
         <router-link to="/">小兔鲜</router-link> 
       </h1>
-      <ul class="navs">
-        <li class="home">
-          <router-link to="/">首页</router-link>
-        </li>
-        <li><a href="#">美食</a></li>
-        <li><a href="#">餐厨</a></li>
-        <li><a href="#">艺术</a></li>
-        <li><a href="#">电器</a></li>
-        <li><a href="#">居家</a></li>
-        <li><a href="#">洗护</a></li>
-        <li><a href="#">孕婴</a></li>
-        <li><a href="#">服装</a></li>
-        <li><a href="#">杂货</a></li>
-      </ul>
+      <!-- Header 部分的 list  -->
+      <AppHeaderNav />
       <div class="search">
         <i class="iconfont icon-search"></i>
         <input type="text" placeholder="搜一搜">
@@ -32,7 +20,7 @@
 </template>
 
 <script lang="ts" setup>
-
+import AppHeaderNav from "./app-header-nav.vue"
 </script>
 
 <style lang="less" scoped>
@@ -57,29 +45,6 @@
         background: url(../assets/images/logo.png) no-repeat center 18px / contain;
       }
 
-    }
-    .navs {
-      width: 820px;
-      display: flex;
-      justify-content: space-around;
-      padding-left: 40px;
-      li {
-        margin-right: 40px;
-        width: 38px;
-        text-align: center;
-        a {
-          font-size: 16px;
-          line-height: 32px;
-          height: 32px;
-          display: inline-block;
-        }
-        &:hover {
-          a {
-            color: @xtxColor;
-            border-bottom: 1px solid @xtxColor;
-          }
-        }
-      }
     }
 
     .search {
