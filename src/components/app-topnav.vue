af36f11972be368ad5632093f64c4f68fa3c0e5c
yellowsea <yunhai0644@163.com>
2022-05-17 15:07:28 +0800

    fix:   TopNav style

--- src/components/app-topnav.vue
+++ src/components/app-topnav.vue
@@ -3,7 +3,7 @@
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <li><a class="clearFirstBorder" href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
         <li><a href="javascript:;">退出登录</a></li>
         <li><a href="javascript:;">请先登录</a></li>
         <li><a href="javascript:;">免费注册</a></li>
@@ -27,11 +27,9 @@
   background: #333;
 
   .container {
-    display: flex;
-    justify-content: right;
     ul {
       display: flex;
-      justify-content: center;
+      justify-content: flex-end;
       align-items: center;
       height: 53px;
 
@@ -41,12 +39,11 @@
           color: #cdcdcd;
           line-height: 1;
           display: inline-block;
-          border-left: 1px solid #666;
+          border-left: 2px solid #666;
+
           i {
             font-size: 14px;
             margin-right: 2px;
-
-
           }
 
           // @xtxColor 必须引入 主题的样式
@@ -54,6 +51,9 @@
             color: @xtxColor;
           }
 
+        }
+        .clearFirstBorder {
+          border-left: none;
         }
       }
 
