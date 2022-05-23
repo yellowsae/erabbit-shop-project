2a2ed47fcfe82d360b1d7156ef0fb4cb65972aa1
yellowsea <yunhai0644@163.com>
2022-05-24 00:02:29 +0800

    chore:  add  sub-skeleton

---
+++ src/views/category/components/sub-skeleton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sub-category">
+    <div class="container">
+      <!-- 面包屑骨架 -->
+      <div class="sub-bread">
+        <XtxSkeleton class="item" width="200px" height="22px" />
+      </div>
+      <!-- 筛选分区骨架 -->
+      <div class="sub-filter">
+        <XtxSkeleton class="item" width="800px" height="40px" />
+        <XtxSkeleton class="item" width="800px" height="40px" />
+        <XtxSkeleton class="item" width="600px" height="40px" />
+        <XtxSkeleton class="item" width="600px" height="40px" />
+        <XtxSkeleton class="item" width="600px" height="40px" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import XtxSkeleton from "@/components/library/xtx-skeleton.vue"
+</script>
+
+<style lang="less" scoped>
+.sub-category{
+  min-height: 1000px;
+  background-color: #f5f5f5;
+}
+.sub-bread {
+  display: flex;
+  padding: 25px 10px;
+}
+.sub-filter {
+  background: #fff;
+  padding: 25px;
+  .xtx-skeleton {
+    padding: 10px 0;
+  }
+}
+</style>
--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="sub-category">
-    <div class="container">
-      <!-- 面包屑导航 -->
-      <SubBread />
-      <!-- 筛选分区 -->
-      <SubFilter :newSubFilter="subFilter"/>
-      <!-- 商品分区 -->
+  <transition name="fade">
+    <div class="sub-category" v-if="subFilter">
+      <div class="container">
+        <!-- 面包屑导航 -->
+        <SubBread />
+        <!-- 筛选分区 -->
+        <!-- <SubFilter :newSubFilter="subFilter"/> -->
+        <!-- 商品分区 -->
+      </div>
     </div>
-  </div>
+    <SubSkeleton v-else></SubSkeleton>
+  </transition>
+
 </template>
 
 <script lang="ts" setup>
 import SubBread from "./components/sub-bread.vue"
-import SubFilter from "./components/sub-filter.vue"
+// import SubFilter from "./components/sub-filter.vue"
+import SubSkeleton from "./components/sub-skeleton.vue"
 
 
 import { findSubFilter } from "@/api/category"
@@ -21,13 +26,14 @@ import { ref, watch, provide } from "vue"
 
 
 const route = useRoute()  // 初始化， 用过 useRouter 获取 path 的ID
-const subFilter = ref([])
+const subFilter = ref<any>([])
 
 // 🔔 watch 监听 id 发生变化，需要重新发送请求
 watch(() => route.params.id, async (id) => {
   // 如果不是二级分类页，就不发送请求
   if (route.path !== `/category/sub/${id}`) return
-
+  // 切换分类时，清空数据，让页面展示骨架效果
+  subFilter.value = null
   // 发送请求接口调用
   const { result } = await findSubFilter(id)
   subFilter.value = result
